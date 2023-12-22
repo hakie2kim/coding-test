127dbe26d7e6153b1ab064e5fa994a459e7c38c3
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2023-12-22 18:24:34 +0900

    [level unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,7 +1,10 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        return IntStream.rangeClosed(start_num, end_num).toArray();
+        int[] arr = new int[end_num - start_num + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = start_num++;
+        }
+        
+        return arr;
     }
 }
