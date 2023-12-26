76c50c4a1b47f0196104d3777bf4da6ca44192d8
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2023-12-26 16:40:57 +0900

    [level unrated] Title: 주사위 게임 3, Time: 0.49 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
@@ -5,28 +5,29 @@ public class Solution {
         int[] dice = {a, b, c, d};
         Arrays.sort(dice);
 
-        // 네 주사위에서 모두 같은 숫자인 경우
+        // 네 주사위에서 모두 같은 숫자인 경우 0123
         if (dice[0] == dice[3]) {
             return dice[0] * 1111;
         }
 
-        // 세 주사위에서 두 숫자가 같은 경우
+        // 세 주사위에서 두 숫자가 같은 경우 012 3, 0 123
         if ((dice[0] == dice[2] && dice[3] != dice[0]) || (dice[1] == dice[3] && dice[0] != dice[1])) {
             int p = dice[0] == dice[2] ? dice[0] : dice[1];
             int q = dice[0] == dice[2] ? dice[3] : dice[0];
             return (10 * p + q) * (10 * p + q);
         }
 
-        // 두 쌍이 같은 숫자인 경우
+        // 두 쌍이 같은 숫자인 경우 01 23
         if ((dice[0] == dice[1] && dice[2] == dice[3]) && (dice[0] != dice[2])) {
             return (dice[0] + dice[2]) * (dice[2] - dice[0]);
         }
 
-        // 두 쌍이 다른 숫자인 경우
+        // 두 쌍이 다른 숫자인 경우 01 2 3 0 12 3 0 1 23
         if ((dice[0] == dice[1] && dice[2] != dice[3]) ||
             (dice[1] == dice[2] && dice[0] != dice[3]) || 
             (dice[2] == dice[3] && dice[0] != dice[1]) 
-           ) {
+           ) 
+        {
             return dice[0] == dice[1] ? dice[2] * dice[3] : (dice[1] == dice[2] ? dice[0] * dice[3] : dice[0] * dice[1]);
         }
 
