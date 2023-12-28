b96b92382caea2b36af0a50393745110f109e3dd
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2023-12-29 08:30:23 +0900

    [level unrated] Title: 문자열 여러 번 뒤집기, Time: 2.22 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -3,7 +3,9 @@ class Solution {
         char[] string = my_string.toCharArray();
         
         for (int[] query : queries) {
-            for (int i = query[0], j = query[1]; i < j; i++, j--) { 
+            // 합이 일정한 인덱스끼리 서로 자리 바꿈을 반복
+            // 예) (0, 7), (1, 6), (2, 5), ...
+            for (int i = query[0], j = query[1]; i <= j; i++, j--) { 
                 char tmp = string[i];
                 string[i] = string[j];
                 string[j] = tmp; 
