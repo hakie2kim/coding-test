021855ca61379ea45f4422ea027c32da2b1d633a
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2023-12-29 17:51:50 +0900

    [level unrated] Title: 접미사 배열, Time: 0.61 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -1,14 +1,24 @@
-import java.util.TreeSet;
+import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
         // 자동 정렬하기 위해 Treeset 사용
-        TreeSet<String> ans = new TreeSet<>();
+        /* TreeSet<String> ans = new TreeSet<>();
         
+        for (int i = 0; i < my_string.length(); i++) {
+            ans.add(my_string.substring(i));
+        } */
+        
+        ArrayList<String> ans = new ArrayList<>();
         for (int i = 0; i < my_string.length(); i++) {
             ans.add(my_string.substring(i));
         }
         
-        return ans.stream().toArray(String[]::new);
+        // return ans.stream().sorted().toArray(String[]::new);
+        
+        // return ans.toArray(new String[ans.size()]);
+        
+        Collections.sort(ans);
+        return ans.toArray(String[]::new);
     }
 }
