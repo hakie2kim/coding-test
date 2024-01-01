aa0150e3e6e516ffbdb59bc1ef7be1e63c983547
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-01 14:40:48 +0900

    [level unrated] Title: 글자 지우기, Time: 0.22 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        
+        StringBuffer answer = new StringBuffer(my_string);
+        
+        // deleteCharAt 바로 사용하면 전체 인덱스 개수가 변경됨
+        for (int index : indices) {
+            answer.setCharAt(index, '0'); 
+        }
+        
+        for (int i = 0; i < answer.length(); i++) {
+            if (answer.charAt(i) == '0')
+                answer.deleteCharAt(i--); 
+        }
+        
+        return new String(answer);
+    }
+}
