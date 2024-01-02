bd5c08c673dbcd478e4cc5924068e9904cbf4430
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-03 08:09:19 +0900

    Update 2의 영역.java
    Add comments

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = {};
+        // arr 중 존재하는 2의 인덱스를 indice에 추가
         ArrayList<Integer> indice = new ArrayList<Integer>();
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 2)
                 indice.add(i);
         }
         
+        /* 인덱스의 사이즈에 따라 케이스를 분류
+           인덱스의 사이즈가 0 또는 1이 아닐 경우는 모두 같은 경우 */
+        int[] answer = {};
         switch(indice.size()) {
             case 0:
                 answer = new int[]{-1};
