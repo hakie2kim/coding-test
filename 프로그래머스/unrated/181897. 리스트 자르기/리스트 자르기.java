d60dee80a0f4f4660bee32aa91257dea0de27d6a
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-02 15:43:10 +0900

    [level unrated] Title: 리스트 자르기, Time: 1.81 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -2,22 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
         int a = slicer[0]; int b = slicer[1]; int c = slicer[2];
-        
+        int[] answer = {};
+         
         switch(n) {
             case 1:
-                return Arrays.copyOfRange(num_list, 0, b+1); // 0번 인덱스부터 b번 인덱스까지
+                answer = Arrays.copyOfRange(num_list, 0, b+1); // 0번 인덱스부터 b번 인덱스까지
+                break;
             case 2:
-                return Arrays.copyOfRange(num_list, a, num_list.length); // a번 인덱스부터 마지막까지
+                answer = Arrays.copyOfRange(num_list, a, num_list.length); // a번 인덱스부터 마지막까지
+                break;
             case 3:
-                return Arrays.copyOfRange(num_list, a, b+1); // a번 인덱스부터 b번 인덱스까지
+                answer = Arrays.copyOfRange(num_list, a, b+1); // a번 인덱스부터 b번 인덱스까지
+                break;
             case 4:
+                ArrayList<Integer> case4 = new ArrayList<Integer>();
                 for (int i = a; i <= b; i += c) {
-                    answer.add(num_list[i]);
+                    case4.add(num_list[i]);
                 }
-                return answer.stream().mapToInt(Integer::intValue).toArray();
+                answer = case4.stream().mapToInt(Integer::intValue).toArray();
+                break;
         }
-        return new int[0];
+        
+        return answer;
     }
 }
