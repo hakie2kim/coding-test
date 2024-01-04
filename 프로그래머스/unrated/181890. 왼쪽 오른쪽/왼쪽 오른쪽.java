3b97caed8e5428cc6ba275e4a918fd8776e1cb5c
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-04 09:00:24 +0900

    Update 왼쪽 오른쪽.java
    Add comments and remove irrelevant codes

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -3,39 +3,22 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] str_list) {
         ArrayList<String> answerL = new ArrayList<String>();
+        
+        // 배열을 하나씩 담으면서 
+        
         for (int i = 0; i < str_list.length; i++) {
+            // l이 나오면 담고 있던 배열을 바로 리턴한다.
             if (str_list[i].equals("l")) {
                 return answerL.stream().toArray(String[]::new);       
             }
             answerL.add(str_list[i]);
             
+            // r이 나오면 그 다음 인덱스부터 끝까지를 리턴한다.
             if (str_list[i].equals("r")) {
                 return Arrays.copyOfRange(str_list, i+1, str_list.length);
             }    
         }
+        
         return new String[]{};
-        // return answer.stream().toArray(String[]::new);   
-        
-        // int str_list_length = str_list.length;
-        // int idxL = 0; int idxR = 0;
-        
-//         for (int i = 0; i < str_list_length; i++) {
-//             if (str_list[i].equals("l")) {
-//                 idxL = i;
-//                 break;                
-//             }
-//         }
-        
-//         for (int i = 0; i < str_list_length; i++) {
-//             if (str_list[i].equals("r")) {
-//                 idxR = i;
-//                 break;
-//             }
-//         }
-
-//         if (idxL == -1 || idxR == -1 || idxL == 0 || idxR == 0 || idxL == str_list_length-1 || idxR == str_list_length-1)
-//             return new String[]{};
-        
-//         return idxL < idxR ? Arrays.copyOfRange(str_list, 0, idxL) : Arrays.copyOfRange(str_list, idxR+1, str_list_length);
     }
 }
