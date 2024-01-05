f91623dbaadc5443ca7072021900aa79de188c3e
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-05 09:10:29 +0900

    [level 1] Title: 평균 일일 대여 요금 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/151136. 평균 일일 대여 요금 구하기/평균 일일 대여 요금 구하기.sql
+++ 프로그래머스/1/151136. 평균 일일 대여 요금 구하기/평균 일일 대여 요금 구하기.sql
@@ -1,4 +1,7 @@
-SELECT ROUND(AVG(DAILY_FEE)) AS "AVERAGE_FEE"
+-- Single quotes are used to denote string literals in SQL
+-- Double quotes, on the other hand, are used to specifically denote identifiers such as table names, column names, or aliases that are case-sensitive or contain special characters or spaces
+
+SELECT ROUND(AVG(DAILY_FEE)) AS AVERAGE_FEE
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE CAR_TYPE = 'SUV'
 ;
