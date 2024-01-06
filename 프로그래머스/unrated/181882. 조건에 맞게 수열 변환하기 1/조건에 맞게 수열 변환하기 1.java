51c3ac05477f9cfb060873099c36540bab4de7f2
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-06 14:11:49 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.49 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(int[] arr) {
-        // for (int num : arr) {
+        // for (int num : arr) { 향상된 for문은 참조가 되지 않음 -> 값 변경 X
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 50) {
                 if (arr[i] % 2 == 0)
