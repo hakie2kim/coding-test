d739b380e4f19cc183c6f3464fdcd6c4139c554a
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-07 15:55:30 +0900

    Update 조건에 맞게 수열 변환하기 2.java

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,15 +1,13 @@
 import java.util.*;
 
 class Solution {
-    private void modifyArr(int[] arr) {
-        
-    }
-    
     public int solution(int[] arr) {        
         int repeat = 0;
+        
         while (true) {
             int[] arrBefore = Arrays.copyOf(arr, arr.length);
             
+            // 문제의 arr 바꾸는 과정
             for (int i = 0; i <= arr.length-1; i++) {
                 if (arr[i] >= 50 && (arr[i] % 2 == 0)) {
                     arr[i] /= 2;
@@ -19,12 +17,15 @@ class Solution {
                 }
             }
             
+            // 변경 전 arrBefore와 변경 후 arr가 같으면 while을 벗어남
+            // 그렇지 않으면 다시 반복함
             if (Arrays.equals(arrBefore, arr)) {
                 break;
             }
             
             repeat++;
         }
+        
         return repeat;
     }
 }
