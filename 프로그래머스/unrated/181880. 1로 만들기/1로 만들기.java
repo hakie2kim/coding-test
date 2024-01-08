cf59418eec403134cee03af0616d118772fffd50
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-08 10:54:03 +0900

    Update 1로 만들기.java

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0; int repeat = 0;
         
         for (int i = 0; i < num_list.length; i++) {
-            while (num_list[i] != 1) {
+            while (num_list[i] != 1) { // 요소의 값이 1이 될 때까지 반복
                 if (num_list[i] % 2 == 0) {
                     num_list[i] /= 2;
                 } else {
