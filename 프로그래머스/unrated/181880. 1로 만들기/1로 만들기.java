8a60902c134a0c4bd1b8776731000408b0f6b81e
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-08 16:21:22 +0900

    [level unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
@@ -1,15 +1,13 @@
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0; int repeat = 0;
+        int repeat = 0;
         
         for (int i = 0; i < num_list.length; i++) {
             while (num_list[i] != 1) { // 요소의 값이 1이 될 때까지 반복
-                if (num_list[i] % 2 == 0) {
-                    num_list[i] /= 2;
-                } else {
+                if (num_list[i] % 2 == 1) 
                     num_list[i]--;
-                    num_list[i] /= 2;
-                }
+                
+                num_list[i] /= 2;
                 repeat++;
             }
         }
--- 프로그래머스/unrated/181880. 1로 만들기/README.md
+++ 프로그래머스/unrated/181880. 1로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 1로 만들기 - 181880 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181880?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181880) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 0.01 ms
+메모리: 75.9 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 10:51:44
+2024년 1월 1일 16:21:18
 
 ### 문제 설명
 
