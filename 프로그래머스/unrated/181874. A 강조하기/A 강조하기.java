9862d4ab4fa342459c9a47da028d5ab14bbb2250
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-10 08:09:43 +0900

    [level unrated] Title: A 강조하기, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        char[] answer = myString.toCharArray();
+        
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] == 'a') {
+                answer[i] -= 32; // 'a' = 97, 'A' = 65
+            } else if (answer[i] != 'A' && 65 <= answer[i] && answer[i] <= 90) {
+                answer[i] += 32;
+            } 
+        }
+        
+        return new String(answer);
+    }
+}
