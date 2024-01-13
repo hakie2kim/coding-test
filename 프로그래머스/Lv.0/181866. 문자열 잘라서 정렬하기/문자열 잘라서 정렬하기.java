f9e02548ec7552f1f2f7d89111fc146ad9111257
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-13 14:57:18 +0900

    [level Lv.0] Title: 문자열 잘라서 정렬하기, Time: 17.91 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/Lv.0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myString) {
-        // System.out.println(Arrays.toString(myString.split("x")));
-        String[] myStringArr = myString.split("x");
+//         String[] myStringArr = myString.split("x");
         
-        ArrayList<String> myStringArrWithoutEmptyStr = new ArrayList<>();
+//         ArrayList<String> myStringArrWithoutEmptyStr = new ArrayList<>();
         
-        for (String myStr : myStringArr) {
-            if (!myStr.isEmpty())
-                myStringArrWithoutEmptyStr.add(myStr);
-        }
+//         for (String myStr : myStringArr) {
+//             if (!myStr.isEmpty())
+//                 myStringArrWithoutEmptyStr.add(myStr);
+//         }
         
-        Collections.sort(myStringArrWithoutEmptyStr);
+//         Collections.sort(myStringArrWithoutEmptyStr);
         
-        return myStringArrWithoutEmptyStr.toArray(String[]::new);
+//         return myStringArrWithoutEmptyStr.toArray(String[]::new);
+        
+        return Arrays.stream(myString.split("x")).filter(str -> !str.isEmpty()).sorted().toArray(String[]::new);
     }
 }
