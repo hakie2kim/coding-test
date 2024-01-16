2995f6b7ea03e7d14845dac7ecaa711f06dd1751
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-17 08:22:42 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/README.md
+++ 프로그래머스/0/181856. 배열 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 비교하기 - 181856 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 0.02 ms
+메모리: 73.3 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 8:12:34
+2024년 1월 3일 8:22:40
 
 ### 문제 설명
 
--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,30 +1,41 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        if (arr1.length > arr2.length) {
-            return 1;
-        } else if (arr1.length < arr2.length) {
-            return -1;
-        } else {
-            int arr1Sum = 0;
-            int arr2Sum = 0;
-            
-            for (int num : arr1) {
-                arr1Sum += num;
-            }
-            
-            for (int num : arr2) {
-                arr2Sum += num;
-            }
-            
-            if (arr1Sum > arr2Sum) {
-                return 1;
-            } else if (arr1Sum < arr2Sum) {
-                return -1;
-            }
+        int answer = Integer.compare(arr1.length, arr2.length);
+
+        if(answer == 0) {
+            answer = Integer.compare(IntStream.of(arr1).sum(), IntStream.of(arr2).sum());
         }
+
+        return answer;
         
-        return 0;
+//         // 두 배열의 길이가 다르다면, 배열의 길이가 긴 쪽이 더 큽니다.
+//         if (arr1.length > arr2.length) {
+//             return 1;
+//         } else if (arr1.length < arr2.length) {
+//             return -1;
+//         // 배열의 길이가 같다면 각 배열에 있는 모든 원소의 합을 비교하여 다르다면 더 큰 쪽이 크고, 같다면 같습니다.
+//         } else {
+//             int arr1Sum = 0;
+//             int arr2Sum = 0;
+            
+//             for (int num : arr1) {
+//                 arr1Sum += num;
+//             }
+            
+//             for (int num : arr2) {
+//                 arr2Sum += num;
+//             }
+            
+//             if (arr1Sum > arr2Sum) {
+//                 return 1;
+//             } else if (arr1Sum < arr2Sum) {
+//                 return -1;
+//             }
+//         }
+        
+//         return 0;
     }
 }
