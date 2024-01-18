0541dcf46fadaaa31314c5c478e924af0dc88405
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-19 08:29:22 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -3,7 +3,7 @@ class Solution {
         int total = 0;
         
         for (char num : num_str.toCharArray()) {
-            total += num - '0';
+            total += num - '0'; // char를 정수로 변환하는 방법
         }
         
         return total;
