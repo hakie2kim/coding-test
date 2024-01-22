61ae596e377fe9ecf9fda7877eae22844bb6abcb
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-23 08:34:52 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 5.48 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/README.md
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 조건에 맞게 수열 변환하기 3 - 181835 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181835?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181835) 
 
 ### 성능 요약
 
-메모리: 108 MB, 시간: 1.91 ms
+메모리: 97.8 MB, 시간: 5.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 8:23:42
+2024년 1월 2일 8:34:48
 
 ### 문제 설명
 
--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -1,13 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr, int k) {
-        for (int i = 0; i < arr.length; i++) {
-            if (k % 2 == 0) {
-                arr[i] += k;    
-            } else {
-                arr[i] *= k;
-            }
-        }
+        // 향상된 for문은 일반적인 for문과 달리 배열이나 컬렉션에 저장된 요소들을 읽어오는 용도로만 사용할 수 있다는 제약 있음
+//         for (int i = 0; i < arr.length; i++) {
+//             if (k % 2 == 0) {
+//                 arr[i] += k;    
+//             } else {
+//                 arr[i] *= k;
+//             }
+//         }
         
-        return arr;
+//         return arr;
+        return Arrays.stream(arr).map(elem -> (k % 2 == 0) ? elem + k : elem * k).toArray();
     }
 }
