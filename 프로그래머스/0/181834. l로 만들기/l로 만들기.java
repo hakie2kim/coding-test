1eeead42d9b9fec243b299333188a13ee0983331
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-23 08:47:13 +0900

    [level 0] Title: l로 만들기, Time: 16.97 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,16 +1,17 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder("");
+//         StringBuilder sb = new StringBuilder("");
         
-        for (char letter : myString.toCharArray()) {
-            // 알파벳 char는 뒤로 갈수록 숫자가 커짐
-            if (letter < 'l') { 
-                sb.append("l");
-            } else {
-                sb.append(letter);
-            }
-        }
+//         for (char letter : myString.toCharArray()) {
+//             // 알파벳 char는 뒤로 갈수록 숫자가 커짐
+//             if (letter < 'l') { 
+//                 sb.append("l");
+//             } else {
+//                 sb.append(letter);
+//             }
+//         }
         
-        return sb.toString();
+//         return sb.toString();
+        return myString.replaceAll("[a-l]", "l");
     }
 }
