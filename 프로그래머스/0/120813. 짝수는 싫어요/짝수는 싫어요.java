65d1af675719b1b168a38014ae7b0c45ca372af3
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-27 10:40:18 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int n) {
+        int size = (n % 2 == 1) ? n / 2 + 1 : n / 2;
+        // 입출력 예시의 2번째 경우 (짝수 이하) : 1번째 경우 (홀수 이하)
+        
+        int[] answer = new int[size];
+        
+        for (int i = 0; i < size; i++) {
+            answer[i] = 2 * i + 1;
+        }
+        
+        return answer;
+    }
+}
