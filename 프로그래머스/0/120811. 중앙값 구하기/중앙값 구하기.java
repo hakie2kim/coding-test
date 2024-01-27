da38f313c49a00cdccfc7785a1f1db8bd52ab4e3
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-27 09:30:47 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.34 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -4,6 +4,6 @@ class Solution {
     public int solution(int[] array) {
         Arrays.sort(array);
         // System.out.println(Arrays.toString(array));
-        return array[array.length / 2];
+        return array[array.length / 2]; // 가운데 값 찾기
     }
 }
