9425299724b6626c59f01e9f0465fbe3d55ff40b
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-01-31 08:45:48 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,13 +1,19 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int n) {
         int sum = 0;
         
-        for (int i = 1; i <= n / 2; i++) { // n/2는 몇 번째 짝수를 의미
-            sum += 2 * i;
+        // for (int i = 1; i <= n / 2; i++) { // n/2는 몇 번째 짝수를 의미
+        //     sum += 2 * i;
+        // }
+        
+        for (int i = 2; i <= n; i+=2) { 
+            sum += i;
         }
         
         return sum;
         
-        // return IntStream(0, n).filter(num -> num % 2 == 0).sum();
+        // return IntStream.rangeClosed(0, n).filter(num -> num % 2 == 0).sum();
     }
 }
