ad5b90af4bf5ffbb5ab226da5ddbe6411787b74c
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-02-15 10:23:16 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.35 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array) {
+        // 가장 큰 수 구하기
+        int[] copyOfArr = Arrays.copyOf(array, array.length);
+        Arrays.sort(copyOfArr);
+        int maxNum = copyOfArr[copyOfArr.length-1];
+        
+        // 가장 큰 수의 인덱스 구하기
+        int maxNumIdx = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == maxNum) {
+                maxNumIdx = i;
+                break;
+            }
+        }
+        
+        return new int[]{maxNum, maxNumIdx};
+    }
+}
