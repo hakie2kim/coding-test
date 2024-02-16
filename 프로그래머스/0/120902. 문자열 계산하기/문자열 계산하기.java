c3cec245606c9106d74b956c3f49b9b1fe3b1cce
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-02-16 09:07:22 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.11 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -4,13 +4,13 @@ class Solution {
         
         int answer = Integer.parseInt(my_str_splitted[0]);
         
-        for (int i = 1; i < my_str_splitted.length; i+=2) {
+        for (int i = 1; i < my_str_splitted.length; i+=2) { // 2번째 원소부터 다음을 반복
             switch(my_str_splitted[i]) {
                 case "+":
-                    answer += Integer.parseInt(my_str_splitted[i+1]);
+                    answer += Integer.parseInt(my_str_splitted[i+1]); // 기존 값에 +
                     break;
                 case "-":
-                    answer -= Integer.parseInt(my_str_splitted[i+1]);
+                    answer -= Integer.parseInt(my_str_splitted[i+1]); // 기존 값에 -
                     break;
             }            
         }
