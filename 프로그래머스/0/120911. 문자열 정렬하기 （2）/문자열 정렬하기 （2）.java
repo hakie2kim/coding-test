15a3d4e556e191412fb25d35cf0c37ccb0f9a816
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-02-20 08:37:07 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.52 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        char[] my_str_to_char_arr = my_string.toLowerCase().toCharArray(); // 모두 소문자로 바꿈
+        Arrays.sort(my_str_to_char_arr); // 알파벳 순으로 정렬
+        
+        return new String(my_str_to_char_arr);
+    }
+}
