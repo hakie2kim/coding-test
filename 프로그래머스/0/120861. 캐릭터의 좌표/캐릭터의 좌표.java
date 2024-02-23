a1873dfad7ae0ccc9069e9f3754989c2dec5a297
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-02-23 09:59:32 +0900

    캐릭터의 좌표.java

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -2,28 +2,28 @@ class Solution {
     public int[] solution(String[] keyinput, int[] board) {
         int[] answer = {0, 0};
         
-        int xMax = (board[0]-1) / 2;
-        int yMax = (board[1]-1) / 2;
+        int xRightMax = (board[0]-1) / 2;
+        int yUpMax = (board[1]-1) / 2;
         
         for (String key : keyinput) {
             switch(key) {
                 case "up":
-                    if (answer[1] >= yMax)
+                    if (answer[1] >= yUpMax) // board의 세로 크기 벗어난 경우
                         break;
                     answer[1]++;
                     break;
                 case "down":
-                    if (answer[1] <= yMax * -1)
+                    if (answer[1] <= yUpMax * -1) // board의 세로 크기 벗어난 경우
                         break;                                       
                     answer[1]--;
                     break; 
                 case "left":
-                    if (answer[0] <= xMax * -1)
+                    if (answer[0] <= xRightMax * -1) // board의 가로 크기 벗어난 경우
                         break;                     
                     answer[0]--;
                     break;
                 case "right":
-                    if (answer[0] >= xMax)
+                    if (answer[0] >= xRightMax) // board의 가로 크기 벗어난 경우
                         break;                    
                     answer[0]++;
                     break;                       
