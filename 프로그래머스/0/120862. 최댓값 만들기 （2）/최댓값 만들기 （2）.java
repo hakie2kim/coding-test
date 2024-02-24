9e794c8146b51d26b37c0d64471b4f941c9fefa0
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-02-24 10:21:01 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.49 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,14 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = numbers[0] * numbers[1];
+//         int answer = numbers[0] * numbers[1];
         
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i+1; j < numbers.length; j++) {
-                if (numbers[i] * numbers[j] > answer) // 모든 곱에 대해서 기존 최댓값보다 크면 해당 곱을 최대값으로 함
-                    answer = numbers[i] * numbers[j];
-            }
-        }
+//         for (int i = 0; i < numbers.length; i++) {
+//             for (int j = i+1; j < numbers.length; j++) {
+//                 if (numbers[i] * numbers[j] > answer) // 모든 곱에 대해서 기존 최댓값보다 크면 해당 곱을 최대값으로 함
+//                     answer = numbers[i] * numbers[j];
+//             }
+//         }
         
-        return answer;
+//         return answer;
+        
+        Arrays.sort(numbers);
+        
+        // 정렬하게 되면 절대값이 가장 큰 값이 왼쪽 또는 오른쪽에 위치하게 됨
+        int firstTwoMultiply = numbers[0] * numbers[1];
+        int lastTwoMultiply = numbers[numbers.length - 2] * numbers[numbers.length - 1];
+        
+        return firstTwoMultiply > lastTwoMultiply ? firstTwoMultiply : lastTwoMultiply;
     }
 }
