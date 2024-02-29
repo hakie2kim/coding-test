3ca78e6cf30683621f5cb8c093ed4164cb9a34e7
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-02-29 09:29:42 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int a, int b) {      
+        int gcd = getGcdByEuclidsAlgorithm(a, b);
+        int bDividedByGcd = b / gcd;
+        
+        while (bDividedByGcd != 1) {
+            if (bDividedByGcd % 2 == 0) {
+                bDividedByGcd /= 2;
+            } else if (bDividedByGcd % 5 == 0) {
+                bDividedByGcd /= 5;
+            } else {
+                return 2;
+            }
+        }
+        
+        return 1;
+    }
+    
+    // 최대공약수 구하기
+    int getGcdByEuclidsAlgorithm(int n1, int n2) {
+        if (n2 == 0) 
+            return n1;
+    
+        return getGcdByEuclidsAlgorithm(n2, n1 % n2);
+    }
+    
+    // 소인수를 List 형태로 갖고 오기
+//     List<Integer> getPrimeFactors(int n) {
+//         List<Integer> primeFactors = new ArrayList<>();
+        
+//         for (int i = 2; i <= n; i++) {
+//             if (n % i == 0) {
+//                 while (n % i == 0) {
+//                     n /= i;
+//                 }
+            
+//                 primeFactors.add(i);                
+//             }
+//         }
+        
+//         return primeFactors;
+//     } 
+}
