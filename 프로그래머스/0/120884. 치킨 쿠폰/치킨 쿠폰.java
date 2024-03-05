4fd30008bc4f12b0fbaa3a23c522ee94cb73ca89
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-03-05 09:43:22 +0900

    Update 치킨 쿠폰.java

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int chicken) { // 주문하는 치킨의 개수
-        int service = 0;
+        int service = 0; // 서비스 치킨의 개수
 
         while (chicken >= 10) {
-            service += chicken / 10;
-            chicken = (chicken / 10) + (chicken % 10);
+            service += chicken / 10; // 받은 쿠폰으로 주문할 수 있는 서비스 치킨
+            chicken = (chicken / 10) + (chicken % 10); // 주문한 후 치킨
         }
         return service;
     }
