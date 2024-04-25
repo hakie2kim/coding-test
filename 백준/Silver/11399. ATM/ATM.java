2312b24f7fc3f2270407ed0bb317ac864b0b7227
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-04-25 09:43:11 +0900

    [Silver IV] Title: ATM, Time: 136 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -5,8 +5,10 @@ import java.util.*;
  
 public class Main {
     static int[] time;
+    static int prevTime = 0;
     static int totalMinTime = 0;
- 
+    
+    // 총 대기시간을 줄이려면 앞 사람이 일찍 끝내야 그만큼 대기시간을 줄일 수 있음
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -18,14 +20,16 @@ public class Main {
             time[i] = Integer.parseInt(st.nextToken());
         }
         
+        // 1. 정렬 
         Arrays.sort(time);
         
+        // 2. i번째 사람이 인출을 끝날 때까지의 시간을 구하기
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j <= i; j++) {
-                totalMinTime += time[j];        
-            }
+            totalMinTime += prevTime + time[i]; // 이전 사람 + 현재 인출하려는 사람
+            prevTime += time[i]; // 이전까지의 누적합
         }
 		
 		System.out.println(totalMinTime);
 	}
+ 
 }
