c61d26485fb30b669b3de146f95e2c6d792e9df4
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-05-24 09:26:47 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1664 ms, Memory: 337072 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,29 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
  
 public class Main {
 	public static void main(String[] args) throws IOException {
     
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
         
         // 수의 개수
 		int N = Integer.parseInt(br.readLine());
 		
-		// 수 배열 초기화
-		int[] arr = new int[N];
-		for(int i = 0; i < N; i++){
-			arr[i] = Integer.parseInt(br.readLine());
+		// 수 카운팅 배열 초기화
+		int[] nums = new int[10001]; // 1 ~ 10000. 맨 앞 0은 무시
+		for(int i = 0; i < N; i++) {
+			nums[Integer.parseInt(br.readLine())]++;
 		}
         
-        // 정렬
-		Arrays.sort(arr);
-        
         // 출력
-		for(int i = 0; i < N; i++){
-			sb.append(arr[i]).append('\n');
+		for(int i = 1; i < 10001; i++){
+		    // i값이 개수가 0이 될 때까지 출력 (빈도수)
+		    while (nums[i]-- > 0) {
+		        sb.append(i).append('\n');
+		    }
 		}
 		System.out.println(sb);
 	}
