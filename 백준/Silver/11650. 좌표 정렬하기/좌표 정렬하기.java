a0b84ee82a0a710dc14b12f2865b9a06b102f263
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-05-27 09:25:33 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 800 ms, Memory: 56476 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] points = new int[N][2];
+		
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+		    st = new StringTokenizer(br.readLine());
+		    points[i][0] = Integer.parseInt(st.nextToken());
+		    points[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(points, (p1, p2) -> {
+		    if (p1[0] != p2[0])
+		        return p1[0] - p2[0];
+		    else
+		        return p1[1] - p2[1];
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+		    sb.append(points[i][0] + " ").append(points[i][1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
