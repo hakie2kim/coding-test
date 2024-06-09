359ba1768c4d45d4535a6367730abeba0d919a43
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-06-09 10:04:52 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,48 +1,77 @@
-import java.util.*;
-
-public class Main
-{
-    private static String[] board;
-    
-    private static int getSolution(int startRow, int startColumn) {
-        String[] orgBlkBoardRow = {"BWBWBWBW", "WBWBWBWB"}; // 블랙 체스판의 각 열 패턴
-        
-        int blackSol = 0;
-        for (int i = startRow; i < startRow+8; i++) {
-		    for (int j = startColumn; j < startColumn+8; j++) {
-		        if (board[i].charAt(j) != orgBlkBoardRow[i%2].charAt(j-startColumn))
-		            blackSol++;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static boolean[][] arr;
+	public static int min = 64;
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+ 
+		arr = new boolean[N][M];
+		
+ 
+		// 배열 입력 
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			
+			for (int j = 0; j < M; j++) {
+				if (str.charAt(j) == 'W') {
+					arr[i][j] = true;		// W일 때는 true 
+				} else {
+					arr[i][j] = false;		// B일 때는 false
+				}
+			}
+		}
+ 
+		for (int i = 0; i <= N - 8; i++) {
+		    for (int j = 0; j <= M - 8; j++) {
+		        find(i, j);
 		    }
 		}
 		
-		// 화이트 체스판의 최소 비용 = (전체 개수 - 블랙 체스판의 최소 비용)
-		return Math.min(blackSol, 64 - blackSol);
-    }
-    
-	public static void main(String[] args) {
-		// input 받기
-		Scanner sc = new Scanner(System.in);
-		int row = sc.nextInt();
-		int column = sc.nextInt();
-		sc.nextLine();
-		
-		board = new String[row];
-		for (int i = 0; i < row; i++) {
-		    board[i] = sc.nextLine();
-		}
-		
-		int sol = Integer.MAX_VALUE;
-		// 체스판 쪼개기, 시작 시점
-		for (int i = 0; i <= row - 8; i++) {
-		    for (int j = 0; j <= column - 8; j++) {
-		        int curSol = getSolution(i, j);
+		System.out.println(min);
+	}
+ 
+	
+	public static void find(int x, int y) {
+	    int endX = x + 8;
+	    int endY = y + 8;
+	    int count = 0;
+	    
+	    boolean WB = arr[x][y]; // 첫 번째 칸의 색
+	    
+	    for (int i = x; i < endX; i++) {
+		    for (int j = y; j < endY; j++) {
 		        
-		        if (curSol < sol) 
-		            sol = curSol;
+		        // 색칠해야 하는 경우
+		        if (arr[i][j] != WB) {
+		            count++;
+		        }
+		        
+		        // 다음 칸은 다른 색이어야 함
+		        WB = !WB;
 		    }
-		}
-		
-		System.out.println(sol);
-		sc.close();
+		    WB = !WB;
+	    }
+	    
+	    /*
+		 *  첫 번째 칸의 색을 기준으로 할 때의 색칠 할 개수(count)와
+		 *  첫 번째 칸의 색과 반대되는 색을 기준으로 할 때의 색칠 할 개수(64 - count) 중 
+		 *  최솟값을 count 에 저장 
+		 */
+	    count = Math.min(count, 64 - count);
+	    
+	    // 최솟값 갱신
+	    min = Math.min(min, count);
 	}
 }
