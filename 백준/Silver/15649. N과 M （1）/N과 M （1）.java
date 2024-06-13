9881c90617bfd07c91549a05c4c9ac455dfcc46c
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-06-13 09:46:09 +0900

    [Silver III] Title: N과 M (1), Time: 276 ms, Memory: 23384 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,40 +1,48 @@
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
 public class Main {
-    static int N, M;
-    static boolean[] visited;
-    static int[] answer;
-    
-    private static void dfs(int depth) {
-        if (depth == M) {
-            for (int elem : answer) {
-                System.out.print(elem + " ");
-            }
-            System.out.println();
-            return;
-        }
-        
-        for (int i = 0; i < N; i++) {
-		    if (visited[i] == false) {
-		        visited[i] = true;
-		        answer[depth] = i+1;
-		        dfs(depth + 1);
-		        visited[i] = false;
-		    }
-		}
-    }
-    
+ 
+	public static int N;	// 정적변수로 변경
+	public static int M;	// 정적변수로 변경
+	public static int[] arr;
+	public static boolean[] visited;
+	public static StringBuilder sb = new StringBuilder();
+ 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
 		StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+		// 정적변수 N과 M을 초기화해준다.
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
+		arr = new int[M];
 		visited = new boolean[N];
-		answer = new int[M];
-		
-		dfs(0);
+        
+		// 정적변수를 쓰면 되기 때문에 굳이 N과 M을 넘겨줄 필요 없다.
+        dfs(0);
+        System.out.println(sb);
+	}
+ 
+	public static void dfs(int depth) {
+	    if (depth == M) {
+	        for (int num : arr) {
+	            sb.append(num).append(' ');
+	        }
+	        sb.append('\n');
+	        return;
+	    }
+	    
+	    for (int i = 0; i < N; i++) {
+	        if (visited[i] == false) {
+	            visited[i] = true;
+	            arr[depth] = i + 1;
+                dfs(depth + 1);
+                visited[i] = false;
+	        }
+	    }
 	}
 }
