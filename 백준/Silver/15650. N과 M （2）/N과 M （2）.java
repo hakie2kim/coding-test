1c8ddec77bfe2731f49c068f730044ed448170ce
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-06-14 09:44:04 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,35 +1,38 @@
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+ 
 public class Main {
-    static int N, M; // 자연수 1부터 N까지 중복 없이 길이가 M인 수열
-    static int[] answer;
-    
-    private static void dfs(int start, int depth) {
+	public static int[] arr;
+	public static int N, M;
+	public static StringBuilder sb = new StringBuilder();
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+ 
+		arr = new int[M];
+        
+		dfs(1, 0);
+		System.out.println(sb);
+	}
+ 
+	public static void dfs(int at, int depth) {
         if (depth == M) {
-            for (int num : answer) {
-                System.out.print(num + " ");
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
             }
-            System.out.println();
-            
+            sb.append('\n');
             return;
         }
         
-        for (int i = start; i < N; i++) {
-            answer[depth] = i+1;
-            dfs(i+1, depth+1);
+        for (int i = at; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
         }
-    }
-    
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-	
-		answer = new int[M];
-		
-		dfs(0, 0);
 	}
 }
