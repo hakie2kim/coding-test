c70d486aa27829f9981c758638930a9db6d25302
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-06-24 09:45:59 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,33 +1,38 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
  
 public class Main {
-    static String[] subtraction;
-    static int sum = Integer.MAX_VALUE; // 초기 상태 여부 확인을 위한 값으로 설정 
-    
-    // 최솟값 -> 먼저 값을 모두 더한 다음 빼야 함
+ 
 	public static void main(String[] args) throws IOException {
+ 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
+		int sum = Integer.MAX_VALUE;	// 초기 상태 여부 확인을 위한 값으로 설정 
+		
 		String[] subtraction = br.readLine().split("-");
 		
-        for (String part : subtraction) {
-            String[] nums = part.split("\\+");
-            
-            int temp = 0;
-            for (String num : nums) {
-                temp += Integer.parseInt(num);
-            }
-            
-            if (sum == Integer.MAX_VALUE) {
-                sum = temp;
-            } else {
-                sum -= temp;
-            }
-        }
-        
-        System.out.println(sum);
+		for (int i = 0; i < subtraction.length; i++) {
+		    int temp = 0;
+		    
+		    // 뺄셈으로 나뉜 토큰을 덧셈으로 분리
+		    String[] addition = subtraction[i].split("\\+");
+		    
+		    // 덧셈으로 나뉜 토큰을 더함
+		    for (int j = 0; j < addition.length; j++) {
+		        temp += Integer.parseInt(addition[j]); 
+		    }
+		    
+		    // 초기 상태인 경우 sum은 첫번째 값
+		    if (sum == Integer.MAX_VALUE) {
+		        sum = temp;
+		    } else {
+		        sum -= temp;
+		    }
+		}
+		
+		System.out.println(sum);
+		
 	}
+ 
 }
